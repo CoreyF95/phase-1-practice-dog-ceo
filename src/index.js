4a05560cf7d323cf0f04a2ca82fad3e6b22bfa1e
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-23 09:20:01 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    
+fetch(imgUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json)
+    })
+
+const config =  {
+    method: "POST",
+    headers: {'Content-Type': 'application/json',
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        imgUrl
+    })
+}
+
+console.log(fetch("file:///Users/corey/Development/Code/1Module/phase-1-practice-dog-ceo/index.html", config)
+)
